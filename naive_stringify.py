1436039ea73a1b189b29169638d4b30fa5e20312
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-08 12:59:50 -0800

    table preproc

--- naive_stringify.py
+++ naive_stringify.py
@@ -18,6 +18,7 @@ def main():
                 for col_cell in range(len(contents['tableData'][row_cell])):
                     col_name = col_order[col_cell]
                     data = contents['tableData'][row_cell][col_cell]['text']
+                    print('data: {}'.format(data))
                     table[col_name].append(data)    
 
             pd.DataFrame.from_dict(table)
