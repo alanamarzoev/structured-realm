e7a4c2bc716558e9e42b6be1286d34f0847d2009
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-07 21:55:14 -0800

    table preproc

--- naive_stringify.py
+++ naive_stringify.py
@@ -3,8 +3,10 @@ import json
 
 def main():
     with open('tables_small.json', 'r') as j:
-        contents = json.loads(j.read())
-        import ipdb; ipdb.set_trace()
+        lines = j.readlines()
+        for line in lines: 
+            contents = json.loads(line)
+            import ipdb; ipdb.set_trace()
 
 
 
