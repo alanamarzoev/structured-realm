3851faac76cd8fc8475a7bec159411858e7e4c35
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-08 08:22:50 -0800

    table preproc

--- naive_stringify.py
+++ naive_stringify.py
@@ -1,13 +1,26 @@
 import json 
-
+import pandas as pd 
 
 def main():
     with open('tables_small.json', 'r') as j:
         lines = j.readlines()
         for line in lines: 
             contents = json.loads(line)
-            import ipdb; ipdb.set_trace()
+            table = {}
+            col_order = []
+            for i, col in enumerate(contents['tableHeaders'][0]): 
+                col_name = col['text']
+                table[col_name] = []
+                col_order.append(col_name)
 
+            for row_cell in range(len(contents)):
+                for col_cell in range(len(contents[0])):
+                    col_name = col_order[col_cell]
+                    data = contents['tableData'][row_cell][col_cell]['text']
+                    table[col_name].append(data)
+
+            pd.DataFrame.from_dict(table)
+            import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__': 
