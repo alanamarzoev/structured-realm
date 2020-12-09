88685d63e5881003902448bb6b3488b26f71880d
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-09 12:18:30 -0800

    STATUS: working data gen from wikitables. need to integrate into realm/retrieval.py

--- language/realm/refresh_doc_embeds.py
+++ language/realm/refresh_doc_embeds.py
@@ -108,7 +108,7 @@ def main(_):
   # doc_shard_sizes = retrieval.count_tf_records_parallel(doc_shard_paths, num_processes=12)
   doc_shard_sizes = [retrieval.count_tf_records(path) for path in doc_shard_paths]
   
-  print(FLAGS.model_dir)
+  print('MODEL DIR: {}'.format(FLAGS.model_dir))
 
   previous_export_path = None
   while True:
