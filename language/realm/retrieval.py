105ddc36b79fd76e1e7e304088ef531a9aa6de0c
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-09 08:37:35 -0800

    STATUS: working data gen from wikitables. need to integrate into realm/retrieval.py

--- language/realm/retrieval.py
+++ language/realm/retrieval.py
@@ -190,6 +190,7 @@ def load_documents(path):
   docs = []
   for val in tf.python_io.tf_record_iterator(path, gzip_option):
     ex = tf.train.Example.FromString(val)
+    print(ex)
     title = get_bytes_feature(ex, 'title')[0]
     body = get_bytes_feature(ex, 'body')[0]
 
