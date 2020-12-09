e0f241dea6e7f46100999f4cba74c647ce4d8f3d
Alana Marzoev <alanamarzoev@gmail.com>
2020-12-08 19:27:44 -0800

    table preproc

--- naive_stringify.py
+++ naive_stringify.py
@@ -21,6 +21,9 @@ from subprocess import check_call
 import numpy as np
 import tensorflow.compat.v1 as tf
 import tensorflow_hub as hub
+tf.compat.v1.enable_eager_execution(
+    config=None, device_policy=None, execution_mode=None
+)
 
 
 def get_dataframes():
